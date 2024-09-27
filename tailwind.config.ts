8675de4c3c7eb296739ebabc0c2f67d66ab681e8
Raavë L. Aires <raave.aires@proton.me>
2024-09-27 16:16:09 +0000

    u

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        cor_de_fundo: "#262626"
       },
     },
   },
